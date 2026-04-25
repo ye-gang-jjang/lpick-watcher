beb008a166b3da7439b3de606cc3394a5ed4d4ff
ye-gang <yegang13209@naver.com>
2026-04-25 15:43:04 +0900

    미사용 레거시 스토어 모듈과 공용 파서 코드 정리

--- lpick_watcher/parsers/__init__.py
+++ lpick_watcher/parsers/__init__.py
@@ -1,3 +1,3 @@
-from .common import build_found_items, looks_like_lp, normalize_ws, parse_price
+from .common import looks_like_lp, normalize_ws, parse_price
 
-__all__ = ["build_found_items", "looks_like_lp", "normalize_ws", "parse_price"]
+__all__ = ["looks_like_lp", "normalize_ws", "parse_price"]
